01fa70da1872419f0a4e99ee03c81bd657cc862b
jrogers <jrogers@tractionondemand.com>
2017-10-22 23:00:24 +0200

    feat(painter): remove black circles

--- app/src/main/java/ca/jdr23bc/simplebackgrounds/painters/CirclePainter.java
+++ app/src/main/java/ca/jdr23bc/simplebackgrounds/painters/CirclePainter.java
@@ -13,24 +13,13 @@ import ca.jdr23bc.simplebackgrounds.utils.RandomUtils;
 public class CirclePainter extends ShapePainter<Circle> {
     private static final String TAG = CirclePainter.class.getCanonicalName();
 
-    Boolean paintBlack;
-
-    public CirclePainter() {
-        paintBlack = RandomUtils.random.nextBoolean();
-    }
-
     @Override
     public void paint(Canvas canvas, Circle circle) {
         PointF center = circle.getCenter();
         Paint paint = getPaint();
-        if (paintBlack) {
-            paint.setColor(Color.BLACK);
-        } else {
-            paint.setColor(getRandomPaintColor());
-        }
+        paint.setColor(getRandomPaintColor());
         Log.d(TAG, "painting color: " + paint.getColor());
         Log.d(TAG, "painting circle: " + circle.toString());
-
         canvas.drawCircle(center.x, center.y, circle.getRadius(), paint);
     }
 }
