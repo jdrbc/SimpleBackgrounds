45de4205549afc7e8b3375fd6ea705481454d6a0
John Rogers <jdr23bc@gmail.com>
2018-12-14 23:06:37 -0800

    save point

--- app/src/main/java/ca/jdr23bc/backgrounds/MainActivity.java
+++ app/src/main/java/ca/jdr23bc/backgrounds/MainActivity.java
@@ -24,20 +24,19 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // for development
-//        setContentView(new BackgroundView(this));
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        launchIntentToSetBackground();
+        // launchIntentToSetBackground();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        closeActivity();
+        // closeActivity();
     }
 
     private void launchIntentToSetBackground() {
@@ -49,8 +48,7 @@ public class MainActivity extends Activity {
             String p = SimpleWallpaperService.class.getPackage().getName();
             String c = SimpleWallpaperService.class.getCanonicalName();
             i.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(p, c));
-        }
-        else{
+        } else {
             i.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
         }
         startActivityForResult(i, 1);
@@ -71,101 +69,5 @@ public class MainActivity extends Activity {
         intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent1.addCategory(Intent.CATEGORY_HOME);
         startActivity(intent1);
-    }
-
-    private static final int LOADING_ANIMATION_STEP_LENGTH_MS = 5;
-    public class BackgroundView extends View {
-        GestureDetector gestureDetector;
-        Background currentBackground;
-        Boolean processing;
-        Boolean doNotStartBackgroundCreation;
-        BackgroundBuilder backgroundBuilder;
-
-        public BackgroundView(Context context) {
-            super(context);
-            processing = false;
-            doNotStartBackgroundCreation = false;
-            gestureDetector = new GestureDetector(context, new GestureListener(this));
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent e) {
-            return gestureDetector.onTouchEvent(e);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            Log.d(TAG, "processing : " + processing);
-
-            if (!processing && !doNotStartBackgroundCreation) {
-                currentBackground = startBackgroundCreation(canvas);
-                processing = true;
-            }
-            doNotStartBackgroundCreation = false;
-            canvas.drawBitmap(currentBackground.getBitmap(), new Matrix(), null);
-        }
-
-        private Background startBackgroundCreation(Canvas canvas) {
-            Background background = new BackgroundFactory().getMultiTreeBackground(canvas.getWidth(), canvas.getHeight());
-//            Background background = new RectBackground(canvas.getWidth(), canvas.getHeight(), new GridLayout());
-            Log.d(TAG, "drawing: " + background.toString());
-            backgroundBuilder = new BackgroundBuilder(this, LOADING_ANIMATION_STEP_LENGTH_MS, background);
-            backgroundBuilder.start();
-            return background;
-        }
-
-        public void onBackgroundCreationComplete() {
-            processing = false;
-            doNotStartBackgroundCreation = true;
-            this.invalidate();
-        }
-
-        private class BackgroundBuilder extends Handler implements Runnable {
-            BackgroundView view;
-            Background background;
-            int delay;
-
-            BackgroundBuilder(BackgroundView view, int delay, Background background) {
-                this.view = view;
-                this.delay = delay;
-                this.background = background;
-            }
-
-            void start() {
-                background.init();
-                post(this);
-            }
-
-            @Override
-            public void run() {
-                if (background.hasNextDrawStep()) {
-                    background.drawStep();
-                    view.invalidate();
-                    postDelayed(this, delay);
-                } else {
-                    view.onBackgroundCreationComplete();
-                }
-            }
-        }
-
-        private class GestureListener extends GestureDetector.SimpleOnGestureListener {
-            View v;
-
-            GestureListener(View v) {
-                this.v = v;
-            }
-
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return true;
-            }
-
-            @Override
-            public boolean onDoubleTap(MotionEvent e) {
-                v.invalidate();
-                return true;
-            }
-        }
     }
 }
